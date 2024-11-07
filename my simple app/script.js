b6ed52a0fa934ac4cbb40684030c2da96d0a8a0f
MukulAnaspure <144963638+MukulAnaspure@users.noreply.github.com>
2024-11-07 11:03:30 +0000

    Update script.js

--- my simple app/script.js
+++ my simple app/script.js
@@ -1,1 +1,4 @@
-
+document.getElementById('myButton').addEventListener('click', function() {
+    const displayText = document.getElementById('displayText');
+    displayText.style.display = 'block';
+});
